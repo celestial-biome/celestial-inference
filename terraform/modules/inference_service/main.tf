80ed17ea57ff408bd08dd4fae3f43d73b8a553eb
celestial-biome <celestialbiome@gmail.com>
2026-01-30 09:43:21 +0900

    fix

--- terraform/modules/inference_service/main.tf
+++ terraform/modules/inference_service/main.tf
@@ -18,7 +18,7 @@ resource "google_cloud_run_v2_service" "inference" {
 
   template {
     containers {
-      image = "asia-northeast1-docker.pkg.dev/${var.project_id}/celestial-inference/inference:latest"
+      image = "asia-northeast1-docker.pkg.dev/${var.project_id}/celestial-inference/inference:${var.image_tag}"
       ports {
         container_port = 8080 # ここをDockerfileの起動ポートと合わせる
       }
