63f3f966ca51fc63f05c9b79755fe97ad614c5fe
celestial-biome <celestialbiome@gmail.com>
2026-02-07 15:22:25 +0900

    fix

--- terraform/environments/staging/main.tf
+++ terraform/environments/staging/main.tf
@@ -6,6 +6,7 @@ module "inference_service" {
   region     = "asia-northeast1"
   image_tag  = var.image_tag
   bq_dataset = "celestial_biome_data_staging"
+  domain_name = "inference-staging.celestial-biome.com"
 }
 
 terraform {
